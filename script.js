b56714813b0c420d95f9e8af4b0277a12b414ad5
vin.xyz <vinzelmarano@gmail.com>
2025-06-20 10:33:33 +0800

    portfolio-website

---
+++ script.js
@@ -0,0 +1,9 @@
+// Para sa credentials ko <:
+function downloadcred() {
+  const link = document.createElement("a");
+  link.href = "resume/vin.xyz-resume.pdf";
+  link.download = "MARAÑO VINZEL JAMES PADILLA";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
